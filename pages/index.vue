3b8d82998527517fc8d9da6178e27d4e5df8e522
kwang89 <sgpioneer@naver.com>
2023-04-02 13:45:37 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <li>
+      <ul>
+        eslint, prettier 설정 :
+        https://dev.to/tao/adding-eslint-and-prettier-to-nuxt-3-2023-5bg
+      </ul>
+    </li>
+  </div>
+</template>
