4df5fa272e60067a9fcc6fd76224cdc942105570
kwang89 <sgpioneer@naver.com>
2023-05-01 22:34:28 +0900

    gitignore에 .vscode추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  // ESLint 옵션을 구성합니다.
-  eslint: {
-    fix: true, // 자동으로 코드 스타일을 고치도록 설정합니다.
-  },
   typescript: {
     strict: true,
   },
